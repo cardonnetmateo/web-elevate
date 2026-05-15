3ecb490f2728af5577a10791f76b2d3c94e8a37c
Mateo Cardonnet <cardonnetmateo@gmail.com>
2026-05-15 12:33:07 -0300

    .

---
+++ src/types/pricing.ts
@@ -0,0 +1,11 @@
+export interface PricingCardProps {
+  title: string;
+  subtitle: string;
+  price: string;
+  priceLabel: string;
+  recurring: string;
+  features: string[];
+  buttonText: string;
+  buttonHref: string;
+  featured?: boolean;
+}
